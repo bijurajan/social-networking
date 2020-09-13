cf1ff81ace28cce57f573d0a5f74e2f7af3bb42e
Biju Rajan <bijurajan78@gmail.com>
2020-09-13 17:35:53 -0400

    Minor refactorings. Pull out into smaller functions

--- src/main/java/kata/Person.java
+++ src/main/java/kata/Person.java
@@ -1,12 +1,11 @@
 package kata;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Person {
-    private List<String> messages = new ArrayList<>();
+    private final List<String> messages = new ArrayList<>();
 
     public Person(String name) {
     }
@@ -19,13 +18,15 @@ public class Person {
         if(person.messages.size() == 1){
             return person.messages;
         }
-
         Collections.reverse(person.messages);
+        return addTimeLineToMessages(person);
+    }
+
+    private List<String> addTimeLineToMessages(Person person) {
         List<String> messagesWithTime = new ArrayList<>();
         for(int i=0; i < person.messages.size(); i++){
             messagesWithTime.add(person.messages.get(i) + " (" + (i+1) +" minute ago)");
         }
         return messagesWithTime;
     }
-
 }
--- src/test/java/kata/FeaturesTest.java
+++ src/test/java/kata/FeaturesTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FeaturesTest {
+
     @Test
     void givenAlicePublishesMessageWhenAliceViewsMessageThenAliceSeesMessage() {
         var person = new Person("Alice");
